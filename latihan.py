6fb9e8cc66b89c0299507a6a1ae2e58e63a5cb2c
dwiamalia123 <dwiamalia228@gmail.com>
2025-12-23 15:37:50 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 
-st.title("Hello, Streamlit!")
+st.title("Hello, Dwi!")
 st.write("This is a simple Streamlit application.")
 
 st.write("Ini contoh *huruf miring*")
